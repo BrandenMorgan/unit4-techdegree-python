cca4db3d8cd4873c75f086191eb4865bc984af65
Branden Morgan <branden.morgan@gmail.com>
2020-06-03 15:31:29 -0700

    Update user input check with regex for price

--- store_inventory/app.py
+++ store_inventory/app.py
@@ -2,7 +2,7 @@ from collections import OrderedDict
 import csv
 import datetime
 import os
-import sys
+import re
 
 from peewee import *
 
@@ -19,18 +19,6 @@ class Product(Model):
 
     class Meta:
         database = db
-
-
-inventory_list = []
-with open('inventory.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        inventory_list.append(dict(row))
-
-for product in inventory_list:
-    product['product_price'] = int(product['product_price'].replace('$', '').replace('.', ''))
-    product['product_quantity'] = int(product['product_quantity'])
-    product['date_updated'] = datetime.datetime.strptime(product['date_updated'], '%m/%d/%Y')
 
 
 def add_to_database(**kwargs):
@@ -54,7 +42,6 @@ def menu_loop():
     choice = None
 
     while choice != 'q':
-        # clear()
         print("\nEnter 'q' to quit.\n")
         for key, value in menu.items():
             print('{}) {}'.format(key, value.__doc__))
@@ -67,8 +54,6 @@ def menu_loop():
                 menu[choice]()
         except ValueError:
             print("\nYou must choose an option from the menu. 'v' 'a' or 'b'.\n")
-
-
 
 
 def view_product():
@@ -87,16 +72,15 @@ def view_product():
                 elif len(str(product.product_price)) < 3:
                      product.product_price = str(product.product_price)[:-1] + '.' + str(product.product_price)[-1:] + '0'
 
-
                 print('\n' + 'Product: ' + product.product_name, '\nQuantity:', product.product_quantity,
-                        '\nPrice: ' + '$', product.product_price, '\nDate added: ', product.date_updated.date().strftime('%m/%d/%Y'), '\n')
-                print('p) view another product')
-                print('q) return to main menu')
-
+                        '\nPrice: ' + '$' + product.product_price, '\nDate added:',
+                            product.date_updated.date().strftime('%m/%d/%Y'), '\n')
+                print('Press "p" to view another product.')
+                print('Any other key to return to main menu.')
             next_action = input('\nAction: ').lower().strip()
             if next_action == 'p':
                 continue
-            if next_action == 'q':
+            if next_action != 'p':
                 break
         except IndexError:
             print("Product of id '{}' not in database. Please enter valid id.\n".format(id))
@@ -108,27 +92,28 @@ def add_product():
     while True:
         try:
             product_name = input('What is the name of the product you would like to add? -> ').title()
-            if not product_name.isalnum():
+            if not product_name.isalpha():
                 raise TypeError
             product_quantity = input('How many would you like to add? -> ')
             if not product_quantity.isnumeric():
                 raise TypeError
             product_price = input('How much does each {} cost? -> $'.format(product_name))
+            if '.' not in product_price:
+                product_price = product_price + '.00'
+            product_price = re.findall(r'[\d]+\.\d\d', product_price)
+            product_price = ('').join(product_price)
             if '.' not in product_price and not product_price.isnumeric():
                 raise TypeError
-            if '.' not in product_price:
-                product_price = product_price + '00'
-            elif '.' in product_price:
+            print('\nProduct:', product_name, '\nQuantity:', product_quantity, '\nPrice: ' + '$' + product_price, '\n')
+            if '.' in product_price:
                 product_price = product_price.replace('.', '')
             add_to_database = input('Add this product to the database? [Yn] ').lower()
-            if add_to_database in ['n', 'no']:
-                break
             if add_to_database in ['y', 'yes']:
                 try:
                     Product.create(product_name=product_name,
                                     product_quantity=product_quantity,
                                     product_price=int(product_price))
-                    print('\Product added successfully!')
+                    print('\nProduct added successfully!')
                     break
                 except IntegrityError:
                     print('{} already exists in the database. Please enter a different product'.format(product_name))
@@ -137,13 +122,16 @@ def add_product():
                     product_record.product_price = product['product_price']
                     product_record.date_updated = product['date_updated']
                     product_record.save()
-                    add_product = input('Would you like to add a product? [y]es/[n]o ').lower()
+                    add_product = input('Would you like to add another product? [y]es/[n]o ').lower()
                     if add_product in ['y', 'yes']:
                         continue
                     else:
                         break
+            else:
+                break
         except TypeError:
-            print("Please make a valid entry.")
+            print("Please make a valid entry.\n")
+
 
 def backup():
     """Back up database"""
@@ -153,6 +141,7 @@ def backup():
                                 Product.product_quantity, Product.date_updated)
         productwriter.writerow(product.keys())
         productwriter.writerows(products.tuples())
+        print("\nBack up successful!")
 
 
 menu = OrderedDict([
@@ -163,6 +152,17 @@ menu = OrderedDict([
 
 
 if __name__ == '__main__':
+    inventory_list = []
+    with open('inventory.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            inventory_list.append(dict(row))
+
+    for product in inventory_list:
+        product['product_price'] = int(product['product_price'].replace('$', '').replace('.', ''))
+        product['product_quantity'] = int(product['product_quantity'])
+        product['date_updated'] = datetime.datetime.strptime(product['date_updated'], '%m/%d/%Y')
+
     db.connect()
     db.create_tables([Product], safe=True)
     add_to_database(product_name=product['product_name'],
