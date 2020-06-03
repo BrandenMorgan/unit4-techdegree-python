93e7b779ab3c7d30adc3a069e0e798590dd286f5
Branden Morgan <branden.morgan@gmail.com>
2020-06-02 18:05:03 -0700

    Add checks for field valdation and user friendly console output

--- store_inventory/app.py
+++ store_inventory/app.py
@@ -55,11 +55,11 @@ def menu_loop():
 
     while choice != 'q':
         # clear()
-        print("Enter 'q' to quit.")
+        print("\nEnter 'q' to quit.\n")
         for key, value in menu.items():
             print('{}) {}'.format(key, value.__doc__))
         try:
-            choice = input('Action: ').lower().strip()
+            choice = input('\nAction: ').lower().strip()
             if choice not in menu and choice != 'q':
                 raise ValueError
             if choice in menu:
@@ -70,49 +70,87 @@ def menu_loop():
 
 
 
-#strip_tz(date_str)
+
 def view_product():
     """View product by id"""
     while True:
         try:
             id = input("Enter the product id that you would like to view. > ")
+            if not id.isnumeric() or id == '0':
+                raise IndexError
             products = Product.select().where(Product.product_id == id)
             if int(id) > Product.select().count():
                 raise IndexError
             for product in products:
-                print('\n' + 'Product: ', product.product_name, product.product_quantity,
-                        product.product_price, product.date_updated, '\n')
-                print('n) view another product')
-                print('d) delete product')
+                if str(product.product_price)[-2:] == '00' or len(str(product.product_price)) >= 3 :
+                    product.product_price = str(product.product_price)[:-2] + '.' + str(product.product_price)[-2:]
+                elif len(str(product.product_price)) < 3:
+                     product.product_price = str(product.product_price)[:-1] + '.' + str(product.product_price)[-1:] + '0'
+
+
+                print('\n' + 'Product: ' + product.product_name, '\nQuantity:', product.product_quantity,
+                        '\nPrice: ' + '$', product.product_price, '\nDate added: ', product.date_updated.date().strftime('%m/%d/%Y'), '\n')
+                print('p) view another product')
                 print('q) return to main menu')
 
-            next_action = input('Action: [Ndq] ').lower().strip()
+            next_action = input('\nAction: ').lower().strip()
+            if next_action == 'p':
+                continue
             if next_action == 'q':
                 break
         except IndexError:
-            print("Product of id {} not in database. Please try again. ".format(id))
+            print("Product of id '{}' not in database. Please enter valid id.\n".format(id))
 
 
 
 def add_product():
     """Add a product"""
-    product_name = input('What is the name of the product you would like to add? -> ')
-    product_quantity = input('How many would you like to add? -> ')
-    product_price = int(input('How much does each {} cost? -> $'.format(product_name)).replace('.', ''))
+    while True:
+        try:
+            product_name = input('What is the name of the product you would like to add? -> ').title()
+            if not product_name.isalnum():
+                raise TypeError
+            product_quantity = input('How many would you like to add? -> ')
+            if not product_quantity.isnumeric():
+                raise TypeError
+            product_price = input('How much does each {} cost? -> $'.format(product_name))
+            if '.' not in product_price and not product_price.isnumeric():
+                raise TypeError
+            if '.' not in product_price:
+                product_price = product_price + '00'
+            elif '.' in product_price:
+                product_price = product_price.replace('.', '')
+            add_to_database = input('Add this product to the database? [Yn] ').lower()
+            if add_to_database in ['n', 'no']:
+                break
+            if add_to_database in ['y', 'yes']:
+                try:
+                    Product.create(product_name=product_name,
+                                    product_quantity=product_quantity,
+                                    product_price=int(product_price))
+                    print('\Product added successfully!')
+                    break
+                except IntegrityError:
+                    print('{} already exists in the database. Please enter a different product'.format(product_name))
+                    product_record = Product.get(product_name=product['product_name'])
+                    product_record.product_quantity = product['product_quantity']
+                    product_record.product_price = product['product_price']
+                    product_record.date_updated = product['date_updated']
+                    product_record.save()
+                    add_product = input('Would you like to add a product? [y]es/[n]o ').lower()
+                    if add_product in ['y', 'yes']:
+                        continue
+                    else:
+                        break
+        except TypeError:
+            print("Please make a valid entry.")
 
-    if input('Add this product to the database? [Yn] ').lower != 'n':
-        Product.create(product_name=product_name,
-                        product_quantity=product_quantity,
-                        product_price=product_price)
-        print('Product added successfully!')
-
-
-# backs up with product_id but has no fieldname
 def backup():
     """Back up database"""
     with open('backup.csv', 'w', newline='') as csvfile:
         productwriter = csv.writer(csvfile)
-        products = Product.select()
+        products = Product.select(Product.product_name, Product.product_price,
+                                Product.product_quantity, Product.date_updated)
         productwriter.writerow(product.keys())
         productwriter.writerows(products.tuples())
 
